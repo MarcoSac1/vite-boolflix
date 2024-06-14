6726cd7138e0564a04273a2a71197533d82f3677
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-14 11:20:56 +0200

    created README, base scaffolding and 2 component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return{
+            
+        }
+    },
+    props: {
+
+
+    }
+}
+</script>
+
+<template>
+<h1>header section</h1>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return{
+            
+        }
+    },
+    props: {
+
+
+    }
+}
+</script>
+
+<template>
+<section>
+    <h1>main section </h1>
+</section>
+</template>
+
+<style lang="scss" scoped>
+</style>
