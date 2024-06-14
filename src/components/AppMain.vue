174c4de5b8683fa12e8ea9d1615603566b6e3000
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-14 17:32:36 +0200

    created new component and for loop to generate searched film

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,25 @@
 <script>
 import AppSearch from "./AppSearch.vue";
 import axios from "axios";
+import CardFilm from "./CardFilm.vue";
 export default {
     components: {
-        AppSearch
+        AppSearch,
+        CardFilm
     },
     data() {
         return{
             film: []
         }
     },
+
     methods:{
         getFilm(filmName){
+            console.log('https://api.themoviedb.org/3/search/movie?api_key=704bcbb605fa3bbfcf7f5616feaf17bb&query='+filmName);
             axios.get('https://api.themoviedb.org/3/search/movie?api_key=704bcbb605fa3bbfcf7f5616feaf17bb&query='+filmName)
             .then((response) => {
-                console.log(response.data.data);
-                this.film=response.data.data;
+                console.log(response);
+                this.film=response.data.results;
             })
             .catch(function(error){
                 console.log(error);
@@ -24,19 +28,21 @@ export default {
                 
             });
         },
-        searchFilm(searchedString){
+        searchFilm(cercato){
+            this.getFilm(cercato);
             console.log('franco');
-            console.log(searchedString);
+            console.log(cercato);
         }
     },
     created() {
-        this.getFilm();
+        
     },
 }
 </script>
 
 <template>
     <AppSearch @searched="searchFilm"/>
+    <CardFilm :film="film"/>
 <section>
     <h1>main section </h1>
 </section>
---
+++ src/components/CardFilm.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return{
+            
+        }
+    },
+    props: {
+        film:{
+            type:Array,
+            required:true
+        }
+
+    }
+}
+</script>
+
+<template>
+    <article v-for="(film, index) in film" :key="index" :film="film">
+        <h1>{{ film.original }}</h1>
+        <h2> {{ film.original_title }}</h2>
+        <p>{{ film.vote_average }}</p> <span> {{ film.original_language }}</span>
+    </article>
+
+
+</template>
+
+<style lang="scss" scoped>
+</style>
