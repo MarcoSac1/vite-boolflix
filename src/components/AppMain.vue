898ec129aa2e16f32ec5b9ee5bddf1c0ef981d82
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-14 16:25:19 +0200

    created search component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,25 +1,38 @@
 <script>
+import AppSearch from "./AppSearch.vue";
 import axios from "axios";
 export default {
+    components: {
+        AppSearch
+    },
     data() {
         return{
-            
+            film: []
         }
     },
     methods:{
-
+        getFilm(){
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=704bcbb605fa3bbfcf7f5616feaf17bb&query=+')
+            .then((response) => {
+                console.log(response.data.data);
+                this.film=response.data.data;
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+            .finally(function () {
+                
+            });
+        },
     },
-    // created:() {
-
-    // },
-    props: {
-
-
-    }
+    created() {
+        this.getFilm();
+    },
 }
 </script>
 
 <template>
+    <AppSearch/>
 <section>
     <h1>main section </h1>
 </section>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return{
+            
+        }
+    },
+    props: {
+
+
+    }
+}
+</script>
+
+<template>
+<h1>search bar section</h1>
+
+<input type="text">
+<button type="button">search</button>
+</template>
+
+<style lang="scss" scoped>
+</style>
